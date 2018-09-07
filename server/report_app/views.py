1306c3e9768c881935c9d62f0059c7d8139ed5e3
Amfoter <khademi72@gmail.com>
2018-09-07 18:35:59 +0430

    changing delete method to truncate for clearing the database truncate resets the ID, delete doesn't

--- server/report_app/views.py
+++ server/report_app/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db import connection
 
 from report_app.models import Point
 from report_app.serializers import PointSerializer
@@ -10,11 +11,13 @@ class PointViewSet(viewsets.ModelViewSet):
     queryset = Point.objects.all()
     serializer_class = PointSerializer
 
+    # adding an action to clear the database
     @action(detail=False, methods=['GET', 'DELETE'])
     def remove_all(self, *args, **kwargs):
-        Point.objects.all().delete()
+        cursor = connection.cursor()
+        cursor.execute("TRUNCATE TABLE public.report_app_point RESTART IDENTITY RESTRICT;")
+        # Point.objects.all().delete()
         return Response('done')
-
 
 # class PointRangeViewSet(viewsets.ModelViewSet):
 #     queryset = PointRange.objects.all()
